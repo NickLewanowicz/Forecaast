b83e317f49e5671df1b8f22ef550ec496153b09d
NickLewanowicz <nicklewanowicz@gmail.com>
2018-10-14 14:58:40 -0400

    Add: private method to fetch the current weather

--- src/components/Home/components/Weather/Weather.tsx
+++ src/components/Home/components/Weather/Weather.tsx
@@ -3,6 +3,11 @@ import './Weather.css'
 
 export default class Weather extends React.Component {
   public render() {
+    const countryCode: string = 'CA'
+    const city: string = 'Ottawa'
+    const unit: string = 'metric'
+    const API_KEY: string = process.env.REACT_APP_WEATHER_API_KEY || '';
+    this.fetchWeather(city, countryCode, unit, API_KEY);
     return (
       <div className="mainContainer">
         <div className="weather">
@@ -12,4 +17,15 @@ export default class Weather extends React.Component {
       </div>
     )
   }
+  private fetchWeather( city: string,countryCode: string, unit:string, apiKey:string):void {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&units=${unit}&appid=${apiKey}`)
+            .then(res => {
+                console.log(res);
+                return res.json()
+             })
+            .then(weather => { 
+                console.log(weather); 
+             });
+
+  } 
 }
