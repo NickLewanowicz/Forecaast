54901feac9c94912d81c05543bfaf79a9544da7f
NickLewanowicz <nicklewanowicz@gmail.com>
2018-10-14 13:34:20 -0400

    Add: Initialize Home component and default / named exports

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+export class Home extends Component {
+  public render() {
+    return (
+      <div>
+        Home Component
+      </div>
+    )
+  }
+}
+
+export default Home
