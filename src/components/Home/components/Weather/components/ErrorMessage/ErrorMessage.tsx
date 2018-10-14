eed56459ec4543518f7a35235bcaedc15edeb651
NickLewanowicz <nicklewanowicz@gmail.com>
2018-10-14 16:28:22 -0400

    Add: init errormessage sub component

---
+++ src/components/Home/components/Weather/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+
+export class ErrorMessage extends React.Component {
+  public render() {
+    return (
+      <div>
+        ErrorMessage
+      </div>
+    )
+  }
+}
+
+export default ErrorMessage
