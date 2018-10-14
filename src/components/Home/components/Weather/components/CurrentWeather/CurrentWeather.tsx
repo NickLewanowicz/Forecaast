c705a673af8cf063c4903d9a375e82fc0751c6bc
NickLewanowicz <nicklewanowicz@gmail.com>
2018-10-14 16:41:09 -0400

    Add: current temperature

--- src/components/Home/components/Weather/components/CurrentWeather/CurrentWeather.tsx
+++ src/components/Home/components/Weather/components/CurrentWeather/CurrentWeather.tsx
@@ -4,11 +4,11 @@ import { CurrentWeatherType } from '../../Weather'
 
 export class CurrentWeather extends React.Component<CurrentWeatherType, {}> {
   public render() {
-    const {city, description} = this.props;
+    const {city, description, temperature} = this.props;
     return (
       <div>
-        <div><em>Currently <span className='description'>{description}</span> in <span className='city'>{city}!</span> </em>
-      </div>
+        <div><em>Currently <span className='description'>{description}</span> in <span className='city'>{city}!</span> </em></div>
+        <div>Its {temperature}°</div>
       </div>
     )
   }
