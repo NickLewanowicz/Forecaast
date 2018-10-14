31485255b098826f5aa436dd8e97780652e8a1db
NickLewanowicz <nicklewanowicz@gmail.com>
2018-10-14 16:36:34 -0400

    Add: expose description and new font color

--- src/components/Home/components/Weather/components/CurrentWeather/CurrentWeather.tsx
+++ src/components/Home/components/Weather/components/CurrentWeather/CurrentWeather.tsx
@@ -4,11 +4,11 @@ import { CurrentWeatherType } from '../../Weather'
 
 export class CurrentWeather extends React.Component<CurrentWeatherType, {}> {
   public render() {
-    const {city} = this.props;
+    const {city, description} = this.props;
     return (
       <div>
-        <div><em>Your Weather is:</em></div>
-        {city}
+        <div><em>Currently <span className='description'>{description}</span> in <span className='city'>{city}!</span> </em>
+      </div>
       </div>
     )
   }
