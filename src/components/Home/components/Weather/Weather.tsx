7ca4d52d609203220a0772c8cd11782669c927d6
NickLewanowicz <nicklewanowicz@gmail.com>
2018-10-14 16:40:59 -0400

    Fix: adjust fixture to celcius

--- src/components/Home/components/Weather/Weather.tsx
+++ src/components/Home/components/Weather/Weather.tsx
@@ -56,7 +56,7 @@ export default class Weather extends React.Component<{}, State> {
         return res.json()
       })
     .then(json => { 
-        json = json.cod === 200 ? json : {"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}],"base":"stations","main":{"temp":280.32,"pressure":1012,"humidity":81,"temp_min":279.15,"temp_max":281.15},"visibility":10000,"wind":{"speed":4.1,"deg":80},"clouds":{"all":90},"dt":1485789600,"sys":{"type":1,"id":5091,"message":0.0103,"country":"GB","sunrise":1485762037,"sunset":1485794875},"id":2643743,"name":"London","cod":200}
+        json = json.cod === 200 ? json : {"coord":{"lon":-0.13,"lat":51.51},"weather":[{"id":300,"main":"Drizzle","description":"light intensity drizzle","icon":"09d"}],"base":"stations","main":{"temp":28.32,"pressure":1012,"humidity":81,"temp_min":27.15,"temp_max":28.15},"visibility":10000,"wind":{"speed":4.1,"deg":80},"clouds":{"all":90},"dt":1485789600,"sys":{"type":1,"id":5091,"message":0.0103,"country":"GB","sunrise":1485762037,"sunset":1485794875},"id":2643743,"name":"London","cod":200}
         const { name: city, 
                 main: {humidity, temp: temperature, temp_max: high, temp_min: low}, 
                 sys: { sunrise, sunset }, 
