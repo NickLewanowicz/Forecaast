fd44d7f3071d878c65cd121bafe3c5540e8cdd85
NickLewanowicz <nicklewanowicz@gmail.com>
2018-10-14 14:10:45 -0400

    Add: style weather component

---
+++ src/components/Home/components/Weather/Weather.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import './Weather.css'
+
+export default class Weather extends React.Component {
+  public render() {
+    return (
+      <div className="mainContainer">
+        <div className="weather">
+          <em>Your Weather is:</em>
+          
+        </div>
+      </div>
+    )
+  }
+}
