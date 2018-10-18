9a96c5de23d198db9f25e94d4cb81da4287657a7
NickLewanowicz <nicklewanowicz@gmail.com>
2018-10-18 14:29:32 -0400

    Fix Localstorage reference in test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,33 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
+const globalAny:any = global;
+
+class LocalStorageMock {
+  public store = {}
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key:any) {
+    return this.store[key] || null;
+  }
+
+  setItem(key:any, value:any) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key:any) {
+    delete this.store[key];
+  }
+};
+
+globalAny.localStorage = new LocalStorageMock;
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
